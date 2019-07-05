bdeacf9a1f3c5730be1a0a17330210d3ea11e39b
Karen Nel <nelk@out-bateleur.co.za>
2019-07-05 11:23:51 +0200

    Add IOS renderers and extensions

---
+++ ThisApp/ThisApp/ThisApp.iOS/Extensions/UIKitExtensions.cs
@@ -0,0 +1,106 @@
+﻿using ThisApp;
+using ThisApp.Controls;
+//using ThisApp.Services.DeviceInfo;
+using CoreAnimation;
+using CoreGraphics;
+using System;
+using System.Drawing;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+namespace ThisApp.iOS.Extensions
+{
+	public static class UIKitExtensions
+	{
+
+		public static void DisposeCustomKeyboard(this UITextField textField)
+		{
+			if (textField == null)
+				return;
+
+			var toolbar = textField.InputAccessoryView;
+			if (toolbar == null)
+				return;
+
+			try
+			{
+				toolbar.Dispose();
+
+				textField.InputAccessoryView = null;
+			}
+			catch
+			{
+			}
+		}
+
+		public static void AddToolbar(this UITextField textField, IKeyboardToolbar keyboardToolbar)
+		{
+			if (textField == null)
+				return;
+
+			try
+			{
+				textField.KeyboardAppearance = UIKeyboardAppearance.Dark;
+
+				var toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, _display.Value.Width, 44.0f))
+				{
+					TintColor = UIColor.White,
+					BarStyle = UIBarStyle.Black,
+					Translucent = true
+				};
+
+				var spaceButton = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace);
+				var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate { textField.ResignFirstResponder(); });
+
+				var previous = keyboardToolbar?.GetPrevious();
+				var next = keyboardToolbar?.GetNext();
+
+				if (keyboardToolbar == null || (previous == null && next == null))
+				{
+					toolbar.Items = new UIBarButtonItem[]
+					{
+						spaceButton,
+						doneButton
+					};
+				}
+				else
+				{
+					if (previous == null)
+						toolbar.Items = new UIBarButtonItem[]
+						{
+							doneButton,
+							spaceButton,
+							new UIBarButtonItem("Next", UIBarButtonItemStyle.Bordered, delegate { next.Focus(); })
+						};
+					else if (next == null)
+						toolbar.Items = new UIBarButtonItem[]
+						{
+							spaceButton,
+							new UIBarButtonItem("Prev", UIBarButtonItemStyle.Bordered, delegate { previous.Focus(); }),
+							doneButton
+						};
+					else
+						toolbar.Items = new UIBarButtonItem[]
+						{
+							doneButton,
+							spaceButton,
+							new UIBarButtonItem("Prev", UIBarButtonItemStyle.Bordered, delegate { previous.Focus(); }),
+							new UIBarButtonItem("Next", UIBarButtonItemStyle.Bordered, delegate { next.Focus(); })
+						};
+				}
+
+				textField.InputAccessoryView = toolbar;
+			}
+			catch
+			{
+			}
+		}
+
+		public static void SubviewsDoNotTranslateAutoresizingMaskIntoConstraints(this UIView view)
+		{
+			foreach (var subview in view.Subviews)
+				subview.TranslatesAutoresizingMaskIntoConstraints = false;
+		}
+	}
+}
---
+++ ThisApp/ThisApp/ThisApp.iOS/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,114 @@
+﻿using ThisApp.Controls;
+using ThisApp.iOS.Renderers;
+using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using ThisApp.iOS.Extensions;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
+
+namespace Bateleur.Platform.iOS.Renderers
+{
+	public class CustomEditorRenderer : EditorRenderer
+	{
+		CustomEditor ElementEx => (CustomEditor)Element;
+
+		UILabel _placeholderLabel;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.OldElement != null)
+			{
+				// Unsubscribe from event handlers and cleanup any resources
+				Control.Ended -= Control_Ended;
+				Control.Changed -= Control_Changed;
+			}
+
+			if (e.NewElement != null)
+			{
+				// Configure the control and subscribe to event handlers
+				CreatePlaceholderLabel();
+
+				Control.Ended += Control_Ended;
+				Control.Changed += Control_Changed;
+
+				SetTextColor();
+			}
+		}
+
+		void Control_Ended(object sender, EventArgs e)
+		{
+			if (!((UITextView)sender).HasText && _placeholderLabel != null)
+				_placeholderLabel.Hidden = false;
+		}
+
+		void Control_Changed(object sender, EventArgs e)
+		{
+			if (_placeholderLabel != null)
+				_placeholderLabel.Hidden = ((UITextView)sender).HasText;
+		}
+
+		void CreatePlaceholderLabel()
+		{
+			_placeholderLabel = new UILabel
+			{
+				BackgroundColor = UIColor.Clear,
+				Font = UIFont.FromName(ElementEx.FontFamily, (nfloat)ElementEx.FontSize)
+			};
+
+			UpdatePlaceholder();
+			UpdatePlaceholderColor();
+
+			_placeholderLabel.SizeToFit();
+
+			Control.AddSubview(_placeholderLabel);
+
+			Control.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+			Control.AddConstraints
+				(
+					_placeholderLabel.AtLeftOf(Control, 7),
+					_placeholderLabel.WithSameCenterY(Control)
+				);
+
+			Control.LayoutIfNeeded();
+
+			_placeholderLabel.Hidden = Control.HasText;
+		}
+
+		void UpdatePlaceholder()
+		{
+			_placeholderLabel.Text = ElementEx.Placeholder;
+		}
+
+		void UpdatePlaceholderColor()
+		{
+			_placeholderLabel.TextColor = ElementEx.PlaceholderColor.ToUIColor();
+		}
+
+		void SetTextColor()
+		{
+			Control.TextColor = Element.TextColor.ToUIColor();
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CustomEditor.PlaceholderProperty.PropertyName)
+				UpdatePlaceholder();
+			else if (e.PropertyName == CustomEditor.PlaceholderColorProperty.PropertyName)
+				UpdatePlaceholderColor();
+			else if
+				(
+					e.PropertyName == Editor.TextColorProperty.PropertyName ||
+					e.PropertyName == VisualElement.IsEnabledProperty.PropertyName
+				)
+				SetTextColor();
+		}
+	}
+}
