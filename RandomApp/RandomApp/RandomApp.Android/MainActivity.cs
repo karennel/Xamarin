deb26bd7e9bdfbacd52b9bcb7aa96cddeea05539
Karen Nel <nelk@out-bateleur.co.za>
2019-07-09 14:28:44 +0200

    Added splash for android

--- RandomApp/RandomApp/RandomApp.Android/MainActivity.cs
+++ RandomApp/RandomApp/RandomApp.Android/MainActivity.cs
@@ -6,28 +6,33 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace RandomApp.Droid
 {
-    [Activity(Label = "RandomApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+	[Activity(Label = "RandomApp",
+					ScreenOrientation = ScreenOrientation.Portrait,
+					Theme = "@style/MainTheme",
+					Icon = "@drawable/icon"
+	)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
+			base.OnCreate(bundle);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			Xamarin.Essentials.Platform.Init(this, bundle);
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+			LoadApplication(new App());
+		}
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
 }
---
+++ RandomApp/RandomApp/RandomApp.Android/SplashActivity.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Util;
+
+namespace RandomApp.Droid
+{
+
+	[Activity
+	 (
+		 Label = "Bateleur",
+		 MainLauncher = true,
+		 NoHistory = true,
+		 ScreenOrientation = ScreenOrientation.Portrait,
+		 Icon = "@drawable/icon",
+		 Theme = "@style/MainTheme.Splash"
+	 )]
+
+	public class SplashActivity : Activity
+	{
+
+		static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+		}
+
+		// Launches the startup task
+		protected override void OnResume()
+		{
+			base.OnResume();
+			Task startupWork = new Task(() => { SimulateStartup(); });
+			startupWork.Start();
+		}
+
+		// Prevent the back button from canceling the startup process
+		public override void OnBackPressed() { }
+
+		// Simulates background work that happens behind the splash screen
+		async void SimulateStartup()
+		{
+			Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+			await Task.Delay(8000); // Simulate a bit of startup work.
+			Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+			
+		}
+
+	}
+}
