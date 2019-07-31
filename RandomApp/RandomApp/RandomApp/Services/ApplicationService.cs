801042f94cf5e65634975e837e8efbadbee95f54
Karen Nel <nelk@out-bateleur.co.za>
2019-07-31 15:05:38 +0200

    adding ApplicationService

---
+++ RandomApp/RandomApp/RandomApp/Services/ApplicationService.cs
@@ -0,0 +1,20 @@
+﻿
+using RandomApp.EndPoints;
+
+namespace RandomApp.Services
+{
+	public abstract class ApplicationService : Service
+	{
+		readonly IEndPointsManager _endPointsManager;
+
+		protected ApplicationService(IEndPointsManager endPointsManager)
+		{
+			_endPointsManager = endPointsManager;
+		}
+
+		protected override string GetEndPoint()
+		{
+			return _endPointsManager.EndPoints.RandomFact.Url;
+		}
+	}
+}
